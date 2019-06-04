742705e4ef9251058940db468227f7f8717bbd08
原　諒太 <ryopityu@yahoo.co.jp>
2019-06-04 20:38:50 +0900

    Lesson6再提出
    Destroyオブジェクトを生成
    MyCameraControllersスクリプトにDestroyに触れたアイテムを破棄する処理を追記しDestroyオブジェクトにアタッチ

--- Assets/MyCameraController.cs
+++ Assets/MyCameraController.cs
@@ -2,24 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MyCameraController : MonoBehaviour {
+public class MyCameraController : MonoBehaviour
+{
     //Unityちゃんのオブジェクト
     private GameObject unitychan;
     //Unityちゃんとカメラの距離
     private float difference;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start()
+    {
         //Unityちゃんのオブジェクトを取得
         this.unitychan = GameObject.Find("unitychan");
         //Unityちゃんとカメラの位置（z位置）の差を求める
         this.difference = unitychan.transform.position.z - this.transform.position.z;
-
     }
-
     // Update is called once per frame
-    void Update () {
+    void Update()
+    {
         //Unityちゃんの位置に合わせてカメラの位置を移動
         this.transform.position = new Vector3(0, this.transform.position.y, this.unitychan.transform.position.z - difference);
-	}
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        //障害物に衝突した場合
+        if (other.gameObject.tag == "CarTag" || other.gameObject.tag == "TrafficConeTag" || other.gameObject.tag == "CoinTag")
+        {
+            //接触したオブジェクトを破棄
+            Destroy(other.gameObject);
+            Debug.Log("Hello");
+        }
+    }
 }
